1f018b5f997522ed70e055d6152316f6222cf190
Eren <b.heyula@hotmail.com>
2022-04-04 11:05:32 +0300

    Add files via upload

---
+++ class/Common/Confirm.php
@@ -0,0 +1,109 @@
+<?php declare(strict_types=1);
+
+namespace XoopsModules\Testimonial\Common;
+
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * Custom form confirm for XOOPS modules
+ *
+ * @copyright     2020 XOOPS Project (https://xoops.org)
+ * @license        GPL 2.0 or later
+ * @package        Testimonial
+ * @since          1.0
+ * @min_xoops      2.5.9
+ * @author         Goffy - Email:<goffy@myxoops.org> - Website:<https://xoops.org>
+ *
+ *
+ * Example:
+    $customConfirm = new Common\Confirm(
+        ['ok' => 1, 'item_id' => $itemId, 'op' => 'delete'],
+        $_SERVER['REQUEST_URI'],
+        \sprintf(\_MA_MYMODULE_FORM_SURE_DELETE,
+        $itemsObj->getCaption()));
+    $form = $customConfirm->getFormConfirm();
+    $GLOBALS['xoopsTpl']->assign('form', $form->render());
+ */
+
+\defined('XOOPS_ROOT_PATH') || die('Restricted access');
+
+/**
+ * Class Object Confirm
+ */
+class Confirm
+{
+    private $hiddens = [];
+    private $action  = '';
+    private $title   = '';
+    private $label   = '';
+    private $object  = '';
+
+    /**
+     * @public function constructor class
+     * @param        $hiddens
+     * @param        $action
+     * @param        $object
+     * @param string $title
+     * @param string $label
+     */
+    public function __construct($hiddens, $action, $object, $title = '', $label = '')
+    {
+        $this->hiddens = $hiddens;
+        $this->action  = $action;
+        $this->object  = $object;
+        $this->title   = $title;
+        $this->label   = $label;
+    }
+
+    /**
+     * @public function getFormConfirm
+     * @return \XoopsThemeForm
+     */
+    public function getFormConfirm()
+    {
+        $moduleDirName      = \basename(__DIR__);
+        $moduleDirNameUpper = \mb_strtoupper($moduleDirName);
+        //in order to be accessable from user and admin area this should be place in language common.php
+        if (!\defined('CO_' . $moduleDirNameUpper . '_DELETE_CONFIRM')) {
+            \define('CO_' . $moduleDirNameUpper . '_DELETE_CONFIRM', 'Confirm delete');
+            \define('CO_' . $moduleDirNameUpper . '_DELETE_LABEL', 'Do you really want to delete:');
+        }
+
+        // Get Theme Form
+        if ('' === $this->action) {
+            $this->action = \Xmf\Request::getString('REQUEST_URI', '', 'SERVER');
+        }
+        if ('' === $this->title) {
+            $this->title = \constant('CO_' . $moduleDirNameUpper . '_DELETE_CONFIRM');
+        }
+        if ('' === $this->label) {
+            $this->label = \constant('CO_' . $moduleDirNameUpper . '_DELETE_LABEL');
+        }
+
+        \xoops_load('XoopsFormLoader');
+        $form = new \XoopsThemeForm($this->title, 'formConfirm', $this->action, 'post', true);
+        $form->setExtra('enctype="multipart/form-data"');
+        $form->addElement(new \XoopsFormLabel($this->label, $this->object));
+        //hiddens
+        foreach ($this->hiddens as $key => $value) {
+            $form->addElement(new \XoopsFormHidden($key, $value));
+        }
+        $form->addElement(new \XoopsFormHidden('ok', 1));
+        $buttonTray = new \XoopsFormElementTray('');
+        $buttonTray->addElement(new \XoopsFormButton('', 'confirm_submit', \_YES, 'submit'));
+        $buttonBack = new \XoopsFormButton('', 'confirm_back', \_NO, 'button');
+        $buttonBack->setExtra('onclick="history.go(-1);return true;"');
+        $buttonTray->addElement($buttonBack);
+        $form->addElement($buttonTray);
+
+        return $form;
+    }
+}
---
+++ class/Common/SysUtility.php
@@ -0,0 +1,215 @@
+<?php declare(strict_types=1);
+
+namespace XoopsModules\Testimonial\Common;
+
+/*
+ Utility Class Definition
+
+ You may not change or alter any portion of this comment or credits of
+ supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit
+ authors.
+
+ This program is distributed in the hope that it will be useful, but
+ WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ */
+
+/**
+ * @license      https://www.fsf.org/copyleft/gpl.html GNU public license
+ * @copyright    https://xoops.org 2000-2020 &copy; XOOPS Project
+ * @author       ZySpec <zyspec@yahoo.com>
+ * @author       Mamba <mambax7@gmail.com>
+ */
+
+use XoopsModules\Testimonial\Helper;
+
+/**
+ * Class Utility
+ */
+class SysUtility
+{
+    use VersionChecks;
+
+    //checkVerXoops, checkVerPhp Traits
+
+    use ServerStats;
+
+    // getServerStats Trait
+
+    use FilesManagement;
+
+    // Files Management Trait
+
+    /**
+     * Access the only instance of this class
+     *
+     * @return object
+     */
+    public static function getInstance()
+    {
+        static $instance;
+        if (null === $instance) {
+            $instance = new static();
+        }
+
+        return $instance;
+    }
+
+    /**
+     * truncateHtml can truncate a string up to a number of characters while preserving whole words and HTML tags
+     * www.gsdesign.ro/blog/cut-html-string-without-breaking-the-tags
+     * www.cakephp.org
+     *
+     * @param string $text         String to truncate.
+     * @param int    $length       Length of returned string, including ellipsis.
+     * @param string $ending       Ending to be appended to the trimmed string.
+     * @param bool   $exact        If false, $text will not be cut mid-word
+     * @param bool   $considerHtml If true, HTML tags would be handled correctly
+     *
+     * @return string Trimmed string.
+     */
+    public static function truncateHtml($text, $length = 100, $ending = '...', $exact = false, $considerHtml = true)
+    {
+        if ($considerHtml) {
+            // if the plain text is shorter than the maximum length, return the whole text
+            if (\mb_strlen(\preg_replace('/<.*?' . '>/', '', $text)) <= $length) {
+                return $text;
+            }
+            // splits all html-tags to scanable lines
+            \preg_match_all('/(<.+?' . '>)?([^<>]*)/s', $text, $lines, \PREG_SET_ORDER);
+            $total_length = \mb_strlen($ending);
+            $open_tags    = [];
+            $truncate     = '';
+            foreach ($lines as $line_matchings) {
+                // if there is any html-tag in this line, handle it and add it (uncounted) to the output
+                if (!empty($line_matchings[1])) {
+                    // if it's an "empty element" with or without xhtml-conform closing slash
+                    if (\preg_match('/^<(\s*.+?\/\s*|\s*(img|br|input|hr|area|base|basefont|col|frame|isindex|link|meta|param)(\s.+?)?)>$/is', $line_matchings[1])) {
+                        // do nothing
+                        // if tag is a closing tag
+                    } elseif (\preg_match('/^<\s*\/([^\s]+?)\s*>$/s', $line_matchings[1], $tag_matchings)) {
+                        // delete tag from $open_tags list
+                        $pos = \array_search($tag_matchings[1], $open_tags, true);
+                        if (false !== $pos) {
+                            unset($open_tags[$pos]);
+                        }
+                        // if tag is an opening tag
+                    } elseif (\preg_match('/^<\s*([^\s>!]+).*?' . '>$/s', $line_matchings[1], $tag_matchings)) {
+                        // add tag to the beginning of $open_tags list
+                        \array_unshift($open_tags, \mb_strtolower($tag_matchings[1]));
+                    }
+                    // add html-tag to $truncate'd text
+                    $truncate .= $line_matchings[1];
+                }
+                // calculate the length of the plain text part of the line; handle entities as one character
+                $content_length = \mb_strlen(\preg_replace('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', ' ', $line_matchings[2]));
+                if ($total_length + $content_length > $length) {
+                    // the number of characters which are left
+                    $left            = $length - $total_length;
+                    $entities_length = 0;
+                    // search for html entities
+                    if (\preg_match_all('/&[0-9a-z]{2,8};|&#[0-9]{1,7};|[0-9a-f]{1,6};/i', $line_matchings[2], $entities, \PREG_OFFSET_CAPTURE)) {
+                        // calculate the real length of all entities in the legal range
+                        foreach ($entities[0] as $entity) {
+                            if ($left >= $entity[1] + 1 - $entities_length) {
+                                $left--;
+                                $entities_length += \mb_strlen($entity[0]);
+                            } else {
+                                // no more characters left
+                                break;
+                            }
+                        }
+                    }
+                    $truncate .= \mb_substr($line_matchings[2], 0, $left + $entities_length);
+                    // maximum lenght is reached, so get off the loop
+                    break;
+                }
+                $truncate     .= $line_matchings[2];
+                $total_length += $content_length;
+
+                // if the maximum length is reached, get off the loop
+                if ($total_length >= $length) {
+                    break;
+                }
+            }
+        } else {
+            if (\mb_strlen($text) <= $length) {
+                return $text;
+            }
+            $truncate = \mb_substr($text, 0, $length - mb_strlen($ending));
+        }
+        // if the words shouldn't be cut in the middle...
+        if (!$exact) {
+            // ...search the last occurance of a space...
+            $spacepos = \mb_strrpos($truncate, ' ');
+            if (isset($spacepos)) {
+                // ...and cut the text in this position
+                $truncate = \mb_substr($truncate, 0, $spacepos);
+            }
+        }
+        // add the defined ending to the text
+        $truncate .= $ending;
+        if ($considerHtml) {
+            // close all unclosed html-tags
+            foreach ($open_tags as $tag) {
+                $truncate .= '</' . $tag . '>';
+            }
+        }
+
+        return $truncate;
+    }
+
+    /**
+     * @param \Xmf\Module\Helper $helper
+     * @param array|null         $options
+     * @return \XoopsFormDhtmlTextArea|\XoopsFormEditor
+     */
+    public static function getEditor($helper = null, $options = null)
+    {
+        /** @var Helper $helper */
+        if (null === $options) {
+            $options           = [];
+            $options['name']   = 'Editor';
+            $options['value']  = 'Editor';
+            $options['rows']   = 10;
+            $options['cols']   = '100%';
+            $options['width']  = '100%';
+            $options['height'] = '400px';
+        }
+
+        if (null === $helper) {
+            $helper = Helper::getInstance();
+        }
+
+        $isAdmin = $helper->isUserAdmin();
+
+        if (\class_exists('XoopsFormEditor')) {
+            if ($isAdmin) {
+                $descEditor = new \XoopsFormEditor(\ucfirst($options['name']), $helper->getConfig('editorAdmin'), $options, $nohtml = false, $onfailure = 'textarea');
+            } else {
+                $descEditor = new \XoopsFormEditor(\ucfirst($options['name']), $helper->getConfig('editorUser'), $options, $nohtml = false, $onfailure = 'textarea');
+            }
+        } else {
+            $descEditor = new \XoopsFormDhtmlTextArea(\ucfirst($options['name']), $options['name'], $options['value'], '100%', '100%');
+        }
+
+        //        $form->addElement($descEditor);
+
+        return $descEditor;
+    }
+
+    /**
+     * @param $fieldname
+     * @param $table
+     *
+     * @return bool
+     */
+    public function fieldExists($fieldname, $table)
+    {
+        global $xoopsDB;
+        $result = $xoopsDB->queryF("SHOW COLUMNS FROM   $table LIKE '$fieldname'");
+
+        return ($xoopsDB->getRowsNum($result) > 0);
+    }
+}
