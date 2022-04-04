f52e94d9707ee2b7707dbe8a04fd2158e846dc93
Eren <b.heyula@hotmail.com>
2022-04-04 11:04:28 +0300

    Add files via upload

---
+++ admin/header.php
@@ -0,0 +1,65 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ You may not change or alter any portion of this comment or credits
+ of supporting developers from this source code or any supporting source code
+ which is considered copyrighted (c) material of the original comment or credit authors.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+*/
+
+/**
+ * Testimonial module for xoops
+ *
+ * @copyright    2021 XOOPS Project (https://xoops.org)
+ * @license      GPL 2.0 or later
+ * @package      testimonial
+ * @since        1.0
+ * @min_xoops    2.5.9
+ * @author       B.Heyula - Email:eren@aymak.net - Website:http://erenyumak.com
+ */
+
+require \dirname(__DIR__, 3) . '/include/cp_header.php';
+require_once \dirname(__DIR__) . '/include/common.php';
+
+$sysPathIcon16   = '../' . $GLOBALS['xoopsModule']->getInfo('sysicons16');
+$sysPathIcon32   = '../' . $GLOBALS['xoopsModule']->getInfo('sysicons32');
+$pathModuleAdmin = $GLOBALS['xoopsModule']->getInfo('dirmoduleadmin');
+$modPathIcon16   = \TESTIMONIAL_URL . '/' . $GLOBALS['xoopsModule']->getInfo('modicons16') . '/';
+$modPathIcon32   = \TESTIMONIAL_URL . '/' . $GLOBALS['xoopsModule']->getInfo('modicons32') . '/';
+
+// Get instance of module
+$helper = \XoopsModules\Testimonial\Helper::getInstance();
+$testimonialsHandler = $helper->getHandler('Testimonials');
+$myts = MyTextSanitizer::getInstance();
+// 
+if (!isset($xoopsTpl) || !\is_object($xoopsTpl)) {
+    require_once \XOOPS_ROOT_PATH . '/class/template.php';
+    $xoopsTpl = new \XoopsTpl();
+}
+
+// Load languages
+\xoops_loadLanguage('admin');
+\xoops_loadLanguage('modinfo');
+
+// Local admin menu class
+if (\file_exists($GLOBALS['xoops']->path($pathModuleAdmin.'/moduleadmin.php'))) {
+    require_once $GLOBALS['xoops']->path($pathModuleAdmin.'/moduleadmin.php');
+} else {
+    \redirect_header('../../../admin.php', 5, \_AM_MODULEADMIN_MISSING);
+}
+
+xoops_cp_header();
+
+// System icons path
+$GLOBALS['xoopsTpl']->assign('sysPathIcon16', $sysPathIcon16);
+$GLOBALS['xoopsTpl']->assign('sysPathIcon32', $sysPathIcon32);
+$GLOBALS['xoopsTpl']->assign('modPathIcon16', $modPathIcon16);
+$GLOBALS['xoopsTpl']->assign('modPathIcon32', $modPathIcon32);
+
+$adminObject = \Xmf\Module\Admin::getInstance();
+$style = \TESTIMONIAL_URL . '/assets/css/admin/style.css';
